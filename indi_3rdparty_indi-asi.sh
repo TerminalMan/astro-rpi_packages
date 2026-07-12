e0b5f5f234f3591abb233266581fb0ab1f8f7df8
TerminalMan <>
2026-07-12 20:30:09 +0200

    asdf

--- indi_3rdparty_indi-asi.sh
+++ indi_3rdparty_indi-asi.sh
@@ -33,6 +33,7 @@ if [[ ! -d indi-3rdparty ]]; then
 	git clone --depth 1 --branch v$VERSION https://github.com/indilib/indi-3rdparty
 fi
 
+cd $BUILDDIR/indi-3rdparty/scripts
 bash indi-3rdparty-deb.sh indi-asi
 
 cp build/indi-asi_*.deb $OUTPUTDIR
